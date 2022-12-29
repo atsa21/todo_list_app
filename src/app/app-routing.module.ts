de0fdf295ed2b22cdf2af32cf0863f0a9f00e301
atsa21 <ts21ann@gmail.com>
2022-12-29 23:13:15 +0200

    bugfix-routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomepageComponent } from './components/homepage/homepage.component';
 import { LoginComponent } from './components/login/login.component';
+import { MainpageComponent } from './components/mainpage/mainpage.component';
 import { SignUpComponent } from './components/sign-up/sign-up.component';
 
 const routes: Routes = [
   {path:'', redirectTo:'login', pathMatch:'full'},
   {path:'login', component: LoginComponent},
   {path:'signup', component: SignUpComponent},
-  {path:'homepage', component: HomepageComponent}
+  {path:'mainpage', component: MainpageComponent}
 ];
 
 @NgModule({
