e22c2d1df0aebfd062b792cd7f38bf65fd48c2c0
atsa21 <ts21ann@gmail.com>
2023-02-02 00:08:49 +0200

    move all duplicated modules in shared moduls

--- src/app/components/mainpage/mainpage.module.ts
+++ src/app/components/mainpage/mainpage.module.ts
@@ -6,27 +6,9 @@ import { TodoListPageComponent } from './todo-list-page/todo-list-page.component
 import { WishListPageComponent } from './wish-list-page/wish-list-page.component';
 import { ProfilePageComponent } from './profile-page/profile-page.component';
 
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatTableModule } from '@angular/material/table';
-import { MatPaginatorModule } from '@angular/material/paginator';
-import { MatSortModule } from '@angular/material/sort';
-import { MatCheckboxModule } from '@angular/material/checkbox';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
-import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
-import { MatDialogModule } from '@angular/material/dialog';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatSelectModule } from '@angular/material/select';
-import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { FormsModule } from '@angular/forms';
+import { SharedModule } from 'src/app/shared/shared.module';
 
-import { ImageCropperModule } from 'ngx-image-cropper';
-import { LottieModule } from 'ngx-lottie';
-import player from 'lottie-web';
-
-export function playerFactory() {
-  return player;
-}
 
 @NgModule({
   declarations: [
@@ -37,22 +19,8 @@ export function playerFactory() {
   imports: [
     CommonModule,
     FormsModule,
-    ReactiveFormsModule,
     MainpageRoutingModule,
-    MatButtonModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatSelectModule,
-    MatIconModule,
-    MatTableModule,
-    MatPaginatorModule,
-    MatSortModule,
-    MatCheckboxModule,
-    MatDialogModule,
-    MatProgressBarModule,
-    MatSnackBarModule,
-    ImageCropperModule,
-    [LottieModule.forRoot({ player: playerFactory })]
+    SharedModule
   ]
 })
 export class MainpageModule { }
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,85 @@
+import { NgModule } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { ImageCropperModule } from 'ngx-image-cropper';
+import { LottieModule } from 'ngx-lottie';
+import player from 'lottie-web';
+
+export function playerFactory() {
+  return player;
+}
+
+@NgModule({
+  imports: [
+    MatToolbarModule,
+    MatIconModule,
+    MatCheckboxModule,
+    MatButtonModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    ReactiveFormsModule,
+    FormsModule,
+    HttpClientModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatMenuModule,
+    MatTabsModule,
+    MatSnackBarModule,
+    MatChipsModule,
+    MatProgressBarModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatProgressSpinnerModule,
+    ImageCropperModule,
+    [LottieModule.forRoot({ player: playerFactory })]
+  ],
+  exports: [
+    MatToolbarModule,
+    MatIconModule,
+    MatCheckboxModule,
+    MatButtonModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    ReactiveFormsModule,
+    FormsModule,
+    HttpClientModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatMenuModule,
+    MatTabsModule,
+    MatSnackBarModule,
+    MatChipsModule,
+    MatProgressBarModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatProgressSpinnerModule,
+    ImageCropperModule,
+    [LottieModule.forRoot({ player: playerFactory })]
+  ]
+})
+export class SharedModule { }
