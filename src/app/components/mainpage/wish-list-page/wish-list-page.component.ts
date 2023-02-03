87d40f8fcd9abed01c8bb082d00eb6b70064cae7
atsa21 <ts21ann@gmail.com>
2023-02-04 01:05:59 +0200

    added wish container

--- src/app/components/mainpage/wish-list-page/wish-list-page.component.ts
+++ src/app/components/mainpage/wish-list-page/wish-list-page.component.ts
@@ -7,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WishListPageComponent implements OnInit {
 
+  public wishList = [
+    { name: 'USD AEON 60 EQT - PINK', price: 299.99 , image: 'https://hedonskate.com/media/cache/usd-aeon-60-eqt-pink-5ae60da4d7c98938742fece6a6ca5b90.jpg' , link: 'https://hedonskate.com/p/skates-1/usd-284/usd-aeon-60-eqt-pink-187078?category%5B0%5D=1&filter-category-95%5B0%5D=aggressive-738&apply=true&category%5B0%5D=1&filter-category-95%5B0%5D=aggressive-738&page=8#.Y92Br3bP1PZ'},
+    { name: 'USD AEON 60 EQT - PINK', price: 299.99 , image: 'https://hedonskate.com/media/cache/usd-aeon-60-eqt-pink-5ae60da4d7c98938742fece6a6ca5b90.jpg' , link: 'https://hedonskate.com/p/skates-1/usd-284/usd-aeon-60-eqt-pink-187078?category%5B0%5D=1&filter-category-95%5B0%5D=aggressive-738&apply=true&category%5B0%5D=1&filter-category-95%5B0%5D=aggressive-738&page=8#.Y92Br3bP1PZ'},
+    { name: 'USD AEON 60 EQT - PINK', price: 299.99 , image: 'https://hedonskate.com/media/cache/usd-aeon-60-eqt-pink-5ae60da4d7c98938742fece6a6ca5b90.jpg' , link: 'https://hedonskate.com/p/skates-1/usd-284/usd-aeon-60-eqt-pink-187078?category%5B0%5D=1&filter-category-95%5B0%5D=aggressive-738&apply=true&category%5B0%5D=1&filter-category-95%5B0%5D=aggressive-738&page=8#.Y92Br3bP1PZ'}
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
