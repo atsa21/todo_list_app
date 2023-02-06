f0a1261747305f65f0378bc22cd0d8dc2565fc4a
atsa21 <ts21ann@gmail.com>
2023-02-07 00:57:28 +0200

    added style to not found page

--- src/app/components/page-not-found/page-not-found.component.ts
+++ src/app/components/page-not-found/page-not-found.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AnimationOptions } from 'ngx-lottie';
 
 @Component({
   selector: 'app-page-not-found',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./page-not-found.component.scss']
 })
 export class PageNotFoundComponent implements OnInit {
+
+  public options: AnimationOptions = {
+    path: '/assets/animation/nowish.json'
+  };
 
   constructor() { }
 
