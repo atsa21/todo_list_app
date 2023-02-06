b5ac39bb3615e5ad1fc252dc56771ebf621aa263
atsa21 <ts21ann@gmail.com>
2023-02-07 00:16:05 +0200

    create page not found component and connect routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { MainpageComponent } from './components/mainpage/mainpage.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 import { SignUpComponent } from './components/sign-up/sign-up.component';
 import { AuthGuardGuard } from './shared/auth-guard.guard';
 
@@ -10,7 +11,8 @@ const routes: Routes = [
   { path:'login', component: LoginComponent },
   { path:'signup', component: SignUpComponent },
   { path:'mainpage', component: MainpageComponent, canActivate: [AuthGuardGuard],
-    loadChildren: () => import('./components/mainpage/mainpage.module').then((mod) => mod.MainpageModule)}
+    loadChildren: () => import('./components/mainpage/mainpage.module').then((mod) => mod.MainpageModule)},
+  { path: '**', component: PageNotFoundComponent }
 ];
 
 @NgModule({
