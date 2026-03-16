ae71d1af7ada5c65b359ba3a6e7f7ae336e6597e
Anna Sakhaudinova <104850911+atsa21@users.noreply.github.com>
2026-03-16 22:46:41 +0100

    FEAT: update-angular-to-16

--- src/app/core/guards/auth/auth.guard.ts
+++ src/app/core/guards/auth/auth.guard.ts
@@ -1,11 +1,11 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, Router } from '@angular/router';
+import { Router } from '@angular/router';
 import { AuthService } from '@core/services/auth/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuard implements CanActivate {
+export class AuthGuard  {
 
   constructor(
     private auth: AuthService,
