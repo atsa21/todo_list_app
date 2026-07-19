c8edef99bae11391329dc5046044d58d4f781f40
ts21ann <ts21ann@gmail.com>
2026-07-19 17:23:44 +0200

    FIX: mobile-styles

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,29 +1,25 @@
-import { AppModule } from './app.module';
-import { RouterTestingModule } from '@angular/router/testing';
-import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { TestBed, } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { provideZonelessChangeDetection } from '@angular/core';
 
 describe('AppComponent', () => {
-  let component: AppComponent;
-  let fixture: ComponentFixture<AppComponent>;
-
   beforeEach(async () => {
-    TestBed.configureTestingModule({
-      imports: [
-        AppModule,
-        RouterTestingModule,
-      ]
-    })
-    .compileComponents();
+    await TestBed.configureTestingModule({
+      imports: [AppComponent],
+      providers: [provideZonelessChangeDetection()]
+    }).compileComponents();
   });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(AppComponent);
-    component = fixture.componentInstance;
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, budget_app');
   });
 });
